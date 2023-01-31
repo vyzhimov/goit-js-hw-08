e85ad2607a4fe658725261660c377440ad72b60a
Alexander Vyzhymov <83233095+vyzhimov@users.noreply.github.com>
2023-01-31 23:44:37 +0200

    finished-second-lesson

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+const getCurrentVideoTime = data => {
+  return localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+};
+
+player.on('timeupdate', throttle(getCurrentVideoTime, 1000));
+
+const startVideoTime = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(startVideoTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
