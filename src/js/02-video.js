5f4942bd51490b6aadc9dd091fc8e6172bb2b311
Alexander Vyzhymov <83233095+vyzhimov@users.noreply.github.com>
2023-01-31 23:48:20 +0200

    fixed-errors

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,14 +4,14 @@ import throttle from 'lodash.throttle';
 const iframe = document.getElementById('vimeo-player');
 const player = new Player(iframe);
 
-const getCurrentVideoTime = data => {
+const setCurrentVideoTime = data => {
   return localStorage.setItem(
     'videoplayer-current-time',
     JSON.stringify(data.seconds)
   );
 };
 
-player.on('timeupdate', throttle(getCurrentVideoTime, 1000));
+player.on('timeupdate', throttle(setCurrentVideoTime, 1000));
 
 const startVideoTime = localStorage.getItem('videoplayer-current-time');
 
