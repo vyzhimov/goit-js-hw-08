ca0df12ea75ed280a54f8ec5f4c84069bc3f3aa0
Alexander Vyzhymov <83233095+vyzhimov@users.noreply.github.com>
2023-02-01 08:54:06 +0200

    finished-third-lesson

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
+fillTextField();
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+// function fillTextField() {
+//   form.message.value = formData.message || '';
+//   form.email.value = formData.email || '';
+// }
+
+function fillTextField() {
+  document.querySelector('textarea').value = formData.message || '';
+  document.querySelector('input').value = formData.email || '';
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
+}
